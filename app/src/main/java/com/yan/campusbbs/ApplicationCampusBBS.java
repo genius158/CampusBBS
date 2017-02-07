2e8f9910c4c60da1ae56c2075d905399416faa24
yan <yxw_developer@outlook.com>
2017-02-07 18:05:21 +0800

    CampusBBS

--- app/src/main/java/com/yan/campusbbs/ApplicationCampusBBS.java
+++ app/src/main/java/com/yan/campusbbs/ApplicationCampusBBS.java
@@ -3,17 +3,14 @@ package com.yan.campusbbs;
 import android.app.Application;
 
 public class ApplicationCampusBBS extends Application {
-
     private ApplicationComponent applicationComponent;
 
     @Override
     public void onCreate() {
         super.onCreate();
-
         applicationComponent = DaggerApplicationComponent.builder()
                 .applicationModule(new ApplicationModule((getApplicationContext())))
                 .build();
-
     }
 
     public ApplicationComponent getApplicationComponent() {
