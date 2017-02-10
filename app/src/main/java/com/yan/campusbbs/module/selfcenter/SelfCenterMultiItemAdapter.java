61553430fca8b5fc4fc57379c0444cf73bec1500
genius158 <yanxianwei1114@gmail.com>
2017-02-11 01:18:05 +0800

    CampusBBs

--- app/src/main/java/com/yan/campusbbs/module/selfcenter/SelfCenterMultiItemAdapter.java
+++ app/src/main/java/com/yan/campusbbs/module/selfcenter/SelfCenterMultiItemAdapter.java
@@ -1,7 +1,6 @@
 package com.yan.campusbbs.module.selfcenter;
 
 import android.content.Context;
-import android.util.Log;
 import android.view.ViewGroup;
 
 import com.chad.library.adapter.base.BaseMultiItemQuickAdapter;
