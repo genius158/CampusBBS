874f098908344e2fba6caaff95ba017a85de39cf
genius158 <yanxianwei1114@gmail.com>
2017-02-12 15:27:27 +0800

    CampusBBS

--- app/src/main/java/com/yan/campusbbs/config/AppSkinConfig.java
+++ app/src/main/java/com/yan/campusbbs/config/AppSkinConfig.java
@@ -8,33 +8,33 @@ import com.yan.campusbbs.R;
 
 public class AppSkinConfig {
     public static final int[] COLOR_PRIMARY_ID = new int[]{
-            R.color.colorPrimary
+            R.color.colorPrimary1
             , R.color.colorPrimary2
             , R.color.colorPrimary3
             , R.color.colorPrimary4
     };
 
     public static final int[] COLOR_PRIMARY_DARK_ID = new int[]{
-            R.color.colorPrimaryDark
+            R.color.colorPrimaryDark1
             , R.color.colorPrimaryDark2
             , R.color.colorPrimaryDark3
             , R.color.colorPrimaryDark4
     };
     public static final int[] COLOR_ACCENT_ID = new int[]{
-            R.color.colorAccent
+            R.color.colorAccent1
             , R.color.colorAccent2
             , R.color.colorAccent3
             , R.color.colorAccent4
     };
     public static final int[] PAGER_TAB_BG = new int[]{
-            R.drawable.study_pager_tab_item_bg
+            R.drawable.study_pager_tab_item_bg1
             , R.drawable.study_pager_tab_item_bg2
             , R.drawable.study_pager_tab_item_bg3
             , R.drawable.study_pager_tab_item_bg4
     };
 
     public static final int[] PAGER_TAB_TEXT_COLOR = new int[]{
-            R.color.campus_pager_tab_title_color
+            R.color.campus_pager_tab_title_color1
             , R.color.campus_pager_tab_title_color2
             , R.color.campus_pager_tab_title_color3
             , R.color.campus_pager_tab_title_color4
