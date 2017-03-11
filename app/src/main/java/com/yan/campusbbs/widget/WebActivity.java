dfa6370e0a9ba6cb93a1a6d56a81a89b4f592718
yan <yxw_developer@outlook.com>
2017-03-11 12:54:34 +0800

    campusBBS

---
+++ app/src/main/java/com/yan/campusbbs/widget/WebActivity.java
@@ -0,0 +1,59 @@
+package com.yan.campusbbs.widget;
+
+import android.support.v4.content.ContextCompat;
+import android.view.View;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+/**
+ * Created by yan on 2017/3/11.
+ */
+
+public class WebActivity {
+//    progressBar1.setMax(100);
+//    progressBar1.setProgressDrawable(
+//            ContextCompat.getDrawable(getBaseContext(),
+//    R.drawable.web_progressbar));
+//
+//    WebSettings webSetting = web.getSettings();
+//    webSetting.setJavaScriptEnabled(true);
+//    web.setWebViewClient(new WebViewClient() {
+//        @Override
+//        public void onReceivedError(WebView webView, int i, String s, String s1) {
+//            super.onReceivedError(webView, i, s, s1);
+//        }
+//
+//        @Override
+//        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+//            view.loadUrl(url);
+//            return true;
+//        }
+//    });
+//    web.setWebChromeClient(new WebChromeClient() {
+//        @Override
+//        public void onReceivedTitle(WebView view, String title) {
+//        }
+//
+//        @Override
+//        public void onProgressChanged(WebView view, int newProgress) {
+//            progressBar1.setProgress(newProgress);
+//            if (progressBar1 != null && newProgress != 100) {
+//                progressBar1.setVisibility(View.VISIBLE);
+//            } else if (progressBar1 != null) {
+//                progressBar1.setVisibility(View.GONE);
+//            }
+//        }
+//    });
+//    webSetting.setAllowFileAccess(true);
+//    webSetting.setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+//    webSetting.setSupportZoom(true);
+////        webSetting.setBuiltInZoomControls(true);
+//    webSetting.setUseWideViewPort(true);
+//    webSetting.setSupportMultipleWindows(false);
+//    webSetting.setAppCacheEnabled(true);
+//    webSetting.setDomStorageEnabled(true);
+//    webSetting.setJavaScriptEnabled(true);
+//
+//    String url = "https://hao.360.cn/?src=lm&ls=n077c1cd59a";
+//    web.loadUrl(url);
+}
